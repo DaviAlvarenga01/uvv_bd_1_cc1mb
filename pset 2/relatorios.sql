4b3232f18fc01a6c3908b256736b55145db35bfd
Davi da Silva Alvarenga <100244955+DaviAlvarenga01@users.noreply.github.com>
2022-05-10 16:36:38 -0300

    Update relatorios.sql

--- pset 2/relatorios.sql
+++ pset 2/relatorios.sql
@@ -1,5 +1,5 @@
 USE UVV
-	 	QUESTÃO 1: 
+	 	-- QUESTÃO 1: 
 SELECT AVG(f.salario) AS mediaSalario, d.nome_departamento  
 FROM  funcionarios f 
 INNER JOIN departamento d 
@@ -7,12 +7,12 @@ ON(d.numero_departamento=f.numero_departamento)
 GROUP BY d.nome_departamento   
 
 
- 		QUESTÃO 2:
+ 		-- QUESTÃO 2:
 SELECT AVG(f.salario) AS mediaSalario, f.sexo 
 FROM funcionarios f 
 GROUP BY f.sexo 
 
-		QUESTÃO 3:
+		-- QUESTÃO 3:
 
 SELECT d.nome_departamento, CONCAT (primeiro_nome,' ', nome_meio, ' ', ultimo_nome) AS nome, FLOOR(DATEDIFF(NOW(), f.data_nascimento) / 365.25) AS idade,f.data_nascimento, f.salario 
 FROM departamento d 
@@ -21,12 +21,26 @@ INNER JOIN
 ON f.numero_departamento = d.numero_departamento 
 
 
-		QUESTÃO 4: ALTERAR 
-SELECT CONCAT (primeiro_nome ,' ', nome_meio, ' ', ultimo_nome) AS nome, FLOOR(DATEDIFF(NOW(), data_nascimento) / 365.25) AS idade, f.data_nascimento, f.salario AS salarioAtual,
+		-- QUESTÃO 4: 
+SELECT CONCAT (primeiro_nome ,' ', nome_meio, ' ', ultimo_nome) AS nome, FLOOR(DATEDIFF(NOW(), data_nascimento) / 365.25) AS idade, f.data_nascimento, 
+f.salario AS salarioAtual,
 CASE WHEN (f.salario < 35) then 20
-ELSE 15
+WHEN (f.salario > 35 ) then 15 
 END AS taxaReajuste,
-case WHEN (f.salario  < 35) then f.salario  + (f.salario  * 0.2)
-ELSE f.salario  + (f.salario  * 0.15)
+CASE WHEN (f.salario  < 35) then  (f.salario  * 0.2)
+ELSE (f.salario  * 0.15) + f.salario
 END AS salarioReajustado
 FROM funcionarios f 
+
+		-- QUESTÃO 5: CÓDIGO NÃO FUNCIONA
+
+WITH gerente AS (SELECT CONCAT (f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) as NomeCompleto, f.cpf, FROM funcionarios f)
+SELECT d.numero_departamento as departamento 
+FROM departamento d
+INNER JOIN funcionario f
+ON f.numero_departamento = d.numero_departamento
+INNER JOIN gerente g ON g.cpf = d.cpf_gerente
+ORDER BY d.nome_departamento ASC, f.salario DESC
+
+
+		-- QUESTÃO 6: 
