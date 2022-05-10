2a2fac6529806e9df67b2594cd6590c6415933a9
Davi da Silva Alvarenga <100244955+DaviAlvarenga01@users.noreply.github.com>
2022-05-10 15:37:25 -0300

    Update relatorios.sql

--- pset 2/relatorios.sql
+++ pset 2/relatorios.sql
@@ -1,52 +1,28 @@
 USE UVV
-
-
 	 	QUESTÃO 1: 
-
-SELECT 
- 	 AVG(f.salario) AS mediaSalario, d.nome_departamento  
-FROM  
-	funcionarios f 
-INNER JOIN 
- 	departamento d 
-ON
- 	(d.numero_departamento=f.numero_departamento)
-GROUP BY 
- 	d.nome_departamento   
+SELECT AVG(f.salario) AS mediaSalario, d.nome_departamento  
+FROM  funcionarios f 
+INNER JOIN departamento d 
+ON(d.numero_departamento=f.numero_departamento)
+GROUP BY d.nome_departamento   
 
 
  		QUESTÃO 2:
-
-
-SELECT
-	 AVG(f.salario) AS mediaSalario, f.sexo 
-FROM 
-	funcionarios f 
-GROUP BY
-	 f.sexo 
-  
+SELECT AVG(f.salario) AS mediaSalario, f.sexo 
+FROM funcionarios f 
+GROUP BY f.sexo 
 
 		QUESTÃO 3:
 
-SELECT 
-	d.nome_departamento, CONCAT (primeiro_nome,' ', nome_meio, ' ', ultimo_nome) AS nome, FLOOR(DATEDIFF(NOW(), f.data_nascimento) / 365.25) AS idade,
-	f.data_nascimento, f.salario 
-
-FROM 
-	 departamento d 
-
+SELECT d.nome_departamento, CONCAT (primeiro_nome,' ', nome_meio, ' ', ultimo_nome) AS nome, FLOOR(DATEDIFF(NOW(), f.data_nascimento) / 365.25) AS idade,f.data_nascimento, f.salario 
+FROM departamento d 
 INNER JOIN 
- 	funcionarios f 
-
-ON 
- 	f.numero_departamento = d.numero_departamento 
-
-
+ funcionarios f 
+ON f.numero_departamento = d.numero_departamento 
 
 
 		QUESTÃO 4: ALTERAR 
-SELECT 
-	 CONCAT (primeiro_nome ,' ', nome_meio, ' ', ultimo_nome) AS nome, FLOOR(DATEDIFF(NOW(), data_nascimento) / 365.25) AS idade, f.data_nascimento, f.salario AS salarioAtual,
+SELECT CONCAT (primeiro_nome ,' ', nome_meio, ' ', ultimo_nome) AS nome, FLOOR(DATEDIFF(NOW(), data_nascimento) / 365.25) AS idade, f.data_nascimento, f.salario AS salarioAtual,
 CASE WHEN (f.salario < 35) then 20
 ELSE 15
 END AS taxaReajuste,
