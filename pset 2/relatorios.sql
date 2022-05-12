8632a0c10d625b263e8b4d951fa91952afa062d3
Davi da Silva Alvarenga <100244955+DaviAlvarenga01@users.noreply.github.com>
2022-05-12 18:31:59 -0300

    Update relatorios.sql

--- pset 2/relatorios.sql
+++ pset 2/relatorios.sql
@@ -35,7 +35,7 @@ FROM funcionarios f
 		-- QUESTÃO 5: NT
 
 WITH gerente AS (SELECT CONCAT (f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) as NomeCompleto, f.cpf, FROM funcionarios f)
-SELECT d.numero_departamento as departamento 
+SELECT d.nome_departamento,
 FROM departamento d
 INNER JOIN funcionario f
 ON f.numero_departamento = d.numero_departamento
@@ -73,4 +73,64 @@ INNER JOIN departamento d on d.numero_departamento = f.numero_departamento
 INNER JOIN trabalha_em t on t.cpf_funcionario = f.CPF
 INNER JOIN projeto p on p.numero_projeto = t.numero_projeto 
 ORDER BY p.numero_projeto 
+
+		--QUESTÃO 9:
+SELECT d.nome_departamento, p.nome_projeto as projeto,
+SUM(DISTINCT(t.horas)) as horasTotais
+FROM trabalha_em te 
+INNER JOIN funcionarios f on  (te.cpf_funcionario = f.CPF) 
+INNER JOIN departamento d on (d.nome_departamento = f.numero_departamento)
+INNER JOIN projeto p on (p.numero_projeto = te.numero_projeto)
+GROUP BY p.nome_projeto 
+
+
+		QUESTÃO 10:
+SELECT AVG(f.salario) AS mediaSalario, d.nome_departamento  
+FROM funcionarios f 
+INNER JOIN departamento d 
+ON(d.numero_departamento=f.numero_departamento)
+GROUP BY d.nome_departamento
+
+   
+ 		--QUESTÃO 11: 
+SELECT  CONCAT (f.primeiro_nome,' ', f.nome_meio,' ',f.ultimo_nome) AS nomeCompleto_funcionario, p.nome_projeto,
+(CASE WHEN t.horas > 0 THEN (t.horas * 50)
+WHEN t.horas < 0 THEN (t.horas * 0)
+END) AS pagamento_Hora 
+FROM funcionarios f 
+INNER JOIN trabalha_em t on t.cpf_funcionario = f.CPF 
+INNER JOIN projeto p on p.numero_projeto = t.numero_projeto 
+
+		
+		--QUESTÃO 12: 
+SELECT d.nome_departamento, CONCAT (f.primeiro_nome,' ', f.nome_meio,' ',f.ultimo_nome) AS nomeCompleto_funcionario, p.nome_projeto, t.horas 
+FROM funcionarios f 
+inner join departamento d on d.numero_departamento = f.numero_departamento 
+inner join trabalha_em t on t.cpf_funcionario = f.CPF 
+inner join projeto p on p.numero_projeto = t.numero_projeto 
+where t.horas = 0 
+ 
+   
+     		--QUESTÃO 13: 
+SELECT CONCAT (f.primeiro_nome,'', f.nome_meio,'', f.ultimo_nome) AS nomeCompleto_funcionario, FLOOR(DATEDIFF(NOW(), f.data_nascimento ) / 365.25) AS idade, f.sexo  
+FROM funcionarios f  
+UNION
+SELECT CONCAT (d.nome_dependente,'', f.nome_meio,'', f.ultimo_nome),FLOOR(DATEDIFF(NOW(), d.data_nascimento ) / 365.25) AS idade, f.sexo  
+FROM dependente d 
+INNER JOIN funcionarios f on f.CPF = d.cpf_funcionario 
+ORDER BY idade DESC
+
+
+		--QUESTÃO 14: 
+SELECT CONCAT (f.primeiro_nome,'', f.nome_meio,'', f.ultimo_nome) AS nomeCompleto_funcionario, dpt.nome_departamento
+FROM funcionarios f 
+INNER JOIN departamento dpt ON dpt.numero_departamento = f.numero_departamento 
+
+
+		--QUESTÃO 15:
+SELECT DISTINCT CONCAT(f.primeiro_nome, ' ', f.nome_meio, '. ', f.ultimo_nome) AS nomeCompleto_funcionario, dp.nome_departamento, p.nome_projeto
+FROM departamento dp
+INNER JOIN funcionarios f ON dp.numero_departamento = f.numero_departamento
+INNER JOIN trabalha_em t ON t.cpf_funcionario = f.CPF 
+INNER JOIN projeto p ON p.numero_projeto = t.numero_projeto
 
